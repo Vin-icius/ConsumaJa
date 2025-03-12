62c9ca4d1192b4d89e3165c95cf7c1bb7b4cd45e
Vinícius Dias Sant'Ana <161242816+Vin-icius@users.noreply.github.com>
2025-03-12 17:33:17 -0300

    fix: atualização do navigator
    Att do navigator para compatibildade com as novas telas

--- consumaja/src/navigation/appNavigator.js
+++ consumaja/src/navigation/appNavigator.js
@@ -3,12 +3,13 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Alert, View, Text, TouchableOpacity, Animated, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
-
+// Importação das telas
 import LoginScreen from '../screens/loginScreen';
 import InicioScreen from '../screens/inicioScreen';
-import OfertasScreen from '../screens/ofertasScreen';
-import EntregasScreen from '../screens/entregasScreen';
 import RelatoriosScreen from '../screens/relatoriosScreen';
+import ConfigScreen from '../screens/configScreen';
+import CadastroEtapa1 from '../screens/CadastroEtapa1';
+import CadastroEtapa2 from '../screens/CadastroEtapa2';
 
 const Stack = createStackNavigator();
 const SCREEN_WIDTH = Dimensions.get('window').width;
@@ -51,17 +52,16 @@ const Sidebar = ({ navigation, isVisible, toggleMenu }) => {
       <TouchableOpacity onPress={toggleMenu} style={{ alignSelf: 'flex-end', marginBottom: 20 }}>
         <Ionicons name="close" size={32} color="white" />
       </TouchableOpacity>
+
       <TouchableOpacity onPress={() => { navigation.navigate('Inicio'); toggleMenu(); }}>
         <Text style={{ fontSize: 18, color: 'white', marginBottom: 15 }}>Início</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => { navigation.navigate('Ofertas'); toggleMenu(); }}>
-        <Text style={{ fontSize: 18, color: 'white', marginBottom: 15 }}>Ofertas</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => { navigation.navigate('Entregas'); toggleMenu(); }}>
-        <Text style={{ fontSize: 18, color: 'white', marginBottom: 15 }}>Entregas</Text>
-      </TouchableOpacity>
+
       <TouchableOpacity onPress={() => { navigation.navigate('Relatorios'); toggleMenu(); }}>
         <Text style={{ fontSize: 18, color: 'white', marginBottom: 15 }}>Relatórios</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => { navigation.navigate('Configurações'); toggleMenu(); }}>
+        <Text style={{ fontSize: 18, color: 'white', marginBottom: 15 }}>Configurações</Text>
       </TouchableOpacity>
       <LogoutButton navigation={navigation} />
     </Animated.View>
@@ -87,9 +87,10 @@ const AppNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Cadastro Etapa 1" component={CadastroEtapa1} />
+      <Stack.Screen name="Cadastro Etapa 2" component={CadastroEtapa2} />
       <Stack.Screen name="Inicio" component={InicioScreen} />
-      <Stack.Screen name="Ofertas" component={OfertasScreen} />
-      <Stack.Screen name="Entregas" component={EntregasScreen} />
+      <Stack.Screen name="Configurações" component={ConfigScreen} />
       <Stack.Screen name="Relatorios" component={RelatoriosScreen} />
       <Stack.Screen name="Dashboard" component={DashboardScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
