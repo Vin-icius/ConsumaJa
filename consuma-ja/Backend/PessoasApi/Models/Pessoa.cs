0ef1c92e3fb4f8bfcff7b90725eae4afa139d8ad
Leon B. Ronchi <leonbronchi@gmail.com>
2025-03-13 09:55:08 -0300

    refractor/feat: MVC e API endereço
    Mudança de padrão de projeto para MVC e start na API de endereços

--- consuma-ja/Backend/PessoasApi/Models/Pessoa.cs
+++ consuma-ja/Backend/PessoasApi/Models/Pessoa.cs
@@ -8,12 +8,15 @@ namespace PessoasApi.Models;
 
 public class Pessoa
 {
+    [Key]
     public int Id {get; set;}
     public String Nome {get; set;}
     public String Email {get; set;}
     public String? Telefone {get; set;}
     public PessoaTipo Tipo {get; set;}
-    public String Login {get; set;}
+    public String? Login {get; set;} // Se for CPF ou CNPJ vai ser o email da pessoa, se for Admin, um valor diferente (ex: nome do admin)
     public String Senha {get; set;}
     public PessoaStatus Status {get; set;}
+    public String? PerfilUrl {get; set;}
+    public ICollection<int> EnderecosID {get; set;}
 }
