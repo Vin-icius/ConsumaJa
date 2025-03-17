7dfa6a08d0970daa97e2e9b60be3471a81967f51
Leon B. Ronchi <leonbronchi@gmail.com>
2025-03-17 09:18:12 -0300

    fix/update: login para API Pessoas
    ainda falta criptografia e descriptografia de senhasme JWT

--- consuma-ja/Backend/PessoasApi/Models/Fisica.cs
+++ consuma-ja/Backend/PessoasApi/Models/Fisica.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace PessoasApi.Models;
 
-public class Fisica : Pessoa
+public class Fisica
 {
     public String CPF {get;set;}
     public Boolean documentoValidado {get; set;}
