32fef64b95cb648db05e32144d31702febdb3f56
Leon B. Ronchi <105613195+LeonRonchi@users.noreply.github.com>
2025-03-21 09:51:54 -0300

    Delete consuma-ja/Backend/PessoasApi directory

--- consuma-ja/Backend/PessoasApi/Models/PessoaStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace PessoasApi.Models
-{
-    public enum PessoaStatus
-    {
-        Inativo = 0,
-        Ativo = 1
-    }
-}
--- consuma-ja/Backend/PessoasApi/Models/PessoaTipo.cs
+++
@@ -1,9 +0,0 @@
-namespace PessoasApi.Models
-{
-    public enum PessoaTipo
-    {
-        Fisica=1,
-        Juridica=2,
-        Admin=3
-    }
-}
