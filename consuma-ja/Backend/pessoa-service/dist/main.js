c9da48e0b687d7420edb7232751e9995ee572768
Leon B. Ronchi <leonbronchi@gmail.com>
2025-04-24 09:53:45 -0300

    fix: correções

--- consuma-ja/Backend/pessoa-service/dist/main.js
+++ consuma-ja/Backend/pessoa-service/dist/main.js
@@ -36,5 +36,5 @@ app.get('/health', (req, res) => { res.status(200).json({ status: 'OK', service:
 app.use((req, res, next) => { next(new app_error_1.AppError(`Rota não encontrada: ${req.originalUrl}`, 404)); });
 app.use(error_middleware_1.errorHandler);
 // --- Iniciar o servidor ---
-const server = app.listen(port, () => { console.log(`[Server] Pessoa Service rodando em http://localhost:${port}`); });
+const server = app.listen(port, () => { console.log(`[Server] Pessoa Service rodando em http://172.20.0.13:${port}`); });
 //# sourceMappingURL=main.js.map
