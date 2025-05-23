d69afdea83ec4b71f9ad3cba270aff2f7cde858b
Leon B. Ronchi <leonbronchi@gmail.com>
2025-05-23 10:13:46 -0300

    fix: correções para VM

--- consuma-ja/Frontend/src/constants/api.js
+++ consuma-ja/Frontend/src/constants/api.js
@@ -1,10 +1,10 @@
-const IP_DA_SUA_MAQUINA = 'localhost';
+const IP_DA_SUA_MAQUINA = 'consumaja.com';
 const LOCATION_SERVICE_PORT = '3001';
 const PRODUCT_SERVICE_PORT = '3000';
 const PERSON_SERVICE_PORT = '3002';
 
-const LOCATION_API_URL = `http://${IP_DA_SUA_MAQUINA}:${LOCATION_SERVICE_PORT}/api/location`;
-const PRODUCT_API_URL = `http://${IP_DA_SUA_MAQUINA}:${PRODUCT_SERVICE_PORT}/api/product`; // <<< Esta URL é usada
-const PERSON_API_URL = `http://${IP_DA_SUA_MAQUINA}:${PERSON_SERVICE_PORT}/api`;
+const LOCATION_API_URL = `https://${IP_DA_SUA_MAQUINA}/api/location`;
+const PRODUCT_API_URL = `https://${IP_DA_SUA_MAQUINA}/api/product`; // <<< Esta URL é usada
+const PERSON_API_URL = `https://${IP_DA_SUA_MAQUINA}/api`;
 
 export { LOCATION_API_URL, PRODUCT_API_URL, PERSON_API_URL };
