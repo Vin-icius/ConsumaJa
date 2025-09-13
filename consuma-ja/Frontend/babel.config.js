7cf159fbf8c286e012d0b766d2c99d2b760d5575
Vinicius Dias Sant'Ana <vinicius28santana@gmail.com>
2025-09-13 14:18:18 -0300

    feat(1): ajuste-front

--- consuma-ja/Frontend/babel.config.js
+++ consuma-ja/Frontend/babel.config.js
@@ -1,11 +1,8 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo",],
+    presets: ['babel-preset-expo'],
     plugins: [
-      ["@locator/babel-jsx/dist", {
-        env: "development",
-      }],
       'react-native-reanimated/plugin',
     ],
   };
